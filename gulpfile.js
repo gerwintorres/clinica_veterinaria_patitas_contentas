5b5e02ffbb542c7114668307d068e2d3a7638740
Juan Camilo Castano <camilo.castano.casas@gmail.com>
2024-04-10 20:37:36 -0500

    Código base

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('cssnano');
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin'); // Minificar imagenes 
+const notify = require('gulp-notify');
+const cache = require('gulp-cache');
+const clean = require('gulp-clean');
+const webp = require('gulp-webp');
+
+const paths = {
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
+    imagenes: 'src/img/**/*'
+}
+
+// Utiliza Sass para compilar archivos .scss en archivos CSS.
+function css() {
+    return src(paths.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        // .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
+}
+
+//Concatena todos los archivos JavaScript en uno solo llamado 'bundle.js'.
+function javascript() {
+    return src(paths.js)
+      .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      .pipe(terser())
+      .pipe(sourcemaps.write('.'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(dest('./build/js'))
+}
+
+// Minifica las imágenes utilizando gulp-imagemin y las guarda en la carpeta 'build/img'.
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(cache(imagemin({ optimizationLevel: 3 })))
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada' }));
+}
+
+// Crea versiones WebP de las imagenes originales y las guarda en la carpeta 'build/img'
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada' }));
+}
+
+// Observa cambios en los archivos SCSS, JavaScript e imagenes.
+function watchArchivos() {
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
+}
+
+// Ejecuta en paralelo las tareas de compilacion de CSS, procesamiento de JavaScript, optimizacion de imagenes y generacion de versiones WebP
+exports.css = css;
+exports.watchArchivos = watchArchivos;
+exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
