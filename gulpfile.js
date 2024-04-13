a0fff9e61e3572a7b4c5cd85c6f91b12ccd2d440
Juan Camilo Castano <camilo.castano.casas@gmail.com>
2024-04-12 20:38:19 -0500

    Pagina principal - Header and Footer

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss')
 const sourcemaps = require('gulp-sourcemaps')
 const cssnano = require('cssnano');
+const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin'); // Minificar imagenes 
