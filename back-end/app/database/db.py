ab441514fe34a9fadebe17d0d63be87bdc344d3f
juan-npablo <juanpablocatano19@gmail.com>
2024-05-28 16:51:36 -0500

    ajuste requirements.txt

--- back-end/app/database/db.py
+++ back-end/app/database/db.py
@@ -7,7 +7,7 @@ Aquí agregan las credenciales de su base de datos local (llamenla bd_clinica), 
 Estructura guía: "mysql+pymysql://user:contrasena@localhost:port/bd_clinica"
 """
 
-engine = create_engine("mysql+pymysql://root:0000.@localhost:3306/bd_clinica")
+engine = create_engine("mysql+pymysql://root:Juanpablopablo19777.@localhost:3306/bd_clinica")
 conn = engine.connect()
 
 meta_data = MetaData()
