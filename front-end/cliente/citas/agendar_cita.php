a91c2d45a23b4b5e23c59573a23296d37152bd2f
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-05-29 18:44:32 -0500

    Formulario Agendar Cita

--- front-end/cliente/citas/agendar_cita.php
+++ front-end/cliente/citas/agendar_cita.php
@@ -9,8 +9,65 @@
     </svg>Ir atrás</a>
 </div>
 
-    
 <h1 class="contenedor titulo-h1-pagina alineacion-izquierda margen-inferior">Agendar cita</h1>
+
+<main class="contenedor formulario-general">
+    <div class="form-imagen-cita"></div>
+    <div class="form-contenido">
+        <form action="">
+            <h3 class="titulo-formulario">Clínica Veterinaria Patitas Contentas requiere la siguiente información</h3>
+            <div class="formulario-datos">
+                <div>
+                    <label for="mascota">Mascota</label>
+                    <select name="mascota" id="mascota" required class="inputs">
+                        <option value="" disabled selected>Seleccione una mascota</option>
+                        <option value="mascota1">Mascota 1</option>
+                        <option value="mascota2">Mascota 2</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="tipoProcedimiento">Tipo de procedimiento</label>
+                    <select name="tipoProcedimiento" id="tipoProcedimiento" required class="inputs">
+                        <option value="" disabled selected>Seleccione una opción</option>
+                        <option value="procedimiento1">Procedimiento 1</option>
+                        <option value="procedimiento2">Procedimiento 2</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="fecha">Fecha</label>
+                    <input type="date" id="fecha" name="fecha" required class="inputs">
+                </div>
+            </div>
+            <div class="formulario-datos">
+                <div>
+                    <label for="hora">Hora</label>
+                    <select name="hora" id="hora" required class="inputs">
+                        <option value="" disabled selected>Seleccione una opción</option>
+                        <option value="hora1">Hora 1</option>
+                        <option value="hora2">Hora 2</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="edad">Edad</label>
+                    <input type="number" id="edad" name="edad" required class="inputs">
+                </div>
+                <div>
+                    <label for="peso">Peso (kg)</label>
+                    <input type="number" id="peso" name="peso" required class="inputs">
+                </div>
+            </div>
+            <div class="formulario-datos">
+                <div>
+                    <label for="raza">Raza</label>
+                    <input type="text" id="raza" name="raza" required class="inputs">
+                </div>
+            </div>
+            <div class="contenido-centrado">
+                <input class="boton-formulario-azul margen-superior" type="submit" value="AGENDAR PROCEDIMIENTO">
+            </div>
+        </form>
+    </div>
+</main>
 
 <?php
     include '../../includes/templates/footer.php';
