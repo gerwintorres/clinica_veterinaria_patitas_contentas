e8ff1bc2016e2165fac3fd16235ef4218c19cdc1
juan-npablo <juanpablocatano19@gmail.com>
2024-05-30 10:53:26 -0500

    cambio en login de cliente

--- back-end/app/routers/rutas_cliente.py
+++ back-end/app/routers/rutas_cliente.py
@@ -3,11 +3,12 @@ from starlette.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
 from sqlalchemy import text 
 from sqlalchemy.orm import Session
 from database.db import conn
-from models.models import clientes
-from schemas.schemas import ClienteSchema, AdministradorSchema, MedicoSchema, CredencialesSchema
+from models.models import clientes, mascotas
+from schemas.schemas import ClienteSchema, CredencialesSchema, MascotaSchema
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from cryptography.fernet import Fernet
+from fastapi.responses import JSONResponse
 
 key = Fernet.generate_key()
 Fernet(key)
@@ -29,17 +30,25 @@ def registrar_cliente(cliente: ClienteSchema):
 
 @router_cliente.post('/login/client')
 def login_cliente(credenciales: CredencialesSchema):
-    query = text(f"SELECT clave FROM cliente WHERE email = '{credenciales.email}'")
-    result = conn.execute(query).fetchone()
-    print(result)
+    query = text(f"SELECT id_cliente, nombre, clave FROM cliente WHERE email = :email")
+    result = conn.execute(query, {"email": credenciales.email}).fetchone()
+    
     if not result:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     
-    clave = result[0]
-    print(clave)
-    #clave_desencriptada = f.decrypt(clave_encriptada.encode()).decode()
+    id_cliente, nombre, clave = result
+
     if credenciales.clave != clave:
-        
         raise HTTPException(status_code=401, detail="Credenciales inválidas")
 
-    return Response(status_code=200)
+    return JSONResponse(content={"id_cliente": id_cliente, "nombre": nombre}, status_code=200)
+
+
+@router_cliente.post("/register/mascota")
+def registrar_mascota(mascota: MascotaSchema):
+    #with engine.connect() as conn:
+    nueva_mascota = mascota.dict()
+    result = conn.execute(mascotas.insert().values(nueva_mascota))
+    conn.commit()
+    print(result)
+    return Response(status_code=HTTP_201_CREATED)
