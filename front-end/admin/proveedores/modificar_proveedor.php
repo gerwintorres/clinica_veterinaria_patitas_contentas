3831489bbf1a8c1a44594cb05557dbb2dc85283d
Camilo Castano <camilo.castano.casas@gmail.com>
2024-06-01 19:02:37 -0500

    Rutas para CRUD de proveedores

--- front-end/admin/proveedores/modificar_proveedor.php
+++ front-end/admin/proveedores/modificar_proveedor.php
@@ -1,8 +1,32 @@
 <?php
     $pagina_actual = '';
+    require '../../config/funciones_proveedores.php';
     include '../../includes/templates/header.php';
-?>
 
+    if(isset($_SESSION['loggedin']) && $_SESSION['usuario'] = 'admin'){
+        $proveedores = obtenerProveedores();
+    }
+
+    $id_proveedor = intval($_GET['id_proveedor']);
+
+    foreach ($proveedores as $proveedor) {
+        if ($proveedor['id_proveedor'] === $id_proveedor) {
+            $nombre = $proveedor['nombre'];
+            $ubicacion = $proveedor['ubicacion'];
+            $email = $proveedor['email'];
+            $telefono = $proveedor['telefono'];
+        }
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nombre = $_POST['nombre'];
+        $apellidos = $_POST['apellidos'];
+        $labor = $_POST['labor'];
+        $telefono = $_POST['telefono'];
+        actualizarProveedor($id_colaborador, $nombre, $apellidos, $labor, $telefono);
+    }
+?>
+ 
 <div class="contenedor contenedor-boton-atras">
     <a href="proveedores.php" class="boton-atras"> <svg width="11" height="20" viewBox="0 0 11 26" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M0.254358 13.9952C-0.0847876 13.5657 -0.0847876 12.8695 0.254358 12.44L9.10815 1.22798C9.4473 0.798517 9.99707 0.798517 10.3362 1.22798L10.7456 1.74641C11.0848 2.17588 11.0848 2.87218 10.7456 3.30166L2.9153 13.2176L10.7456 23.1337C11.0848 23.5631 11.0848 24.2593 10.7456 24.6888L10.3362 25.2073C9.99707 25.6368 9.4473 25.6368 9.10815 25.2073L0.254358 13.9952Z" fill="#231F57"/>
@@ -14,26 +38,26 @@
 <main class="contenedor formulario-general">
     <div class="form-imagen-proveedor"></div>
     <div class="form-contenido">
-        <form action="">
+        <form action="" method="POST">
             <h3 class="titulo-formulario">Clínica Veterinaria Patitas Contentas requiere la siguiente información</h3>
             <div class="formulario-datos">
                 <div>
                     <label for="nombre">Nombre del proveedor</label>
-                    <input type="text" id="nombre" name="nombre" required class="inputs">
+                    <input type="text" id="nombre" name="nombre" value="<?php echo $nombre?>" required class="inputs">
                 </div>
                 <div>
                     <label for="ubicacion">Ubicación</label>
-                    <input type="text" id="ubicacion" name="ubicacion" required class="inputs">
+                    <input type="text" id="ubicacion" name="ubicacion" value="<?php echo $ubicacion?>" required class="inputs">
                 </div>
                 <div>
                     <label for="email">Correo electrónico</label>
-                    <input type="email" id="email" name="email" required class="inputs">
+                    <input type="email" id="email" name="email" required value="<?php echo $email?>" class="inputs">
                 </div>
             </div>
             <div class="formulario-datos">
                 <div>
                     <label for="telefono">Teléfono</label>
-                    <input type="number" id="telefono" name="telefono" required class="inputs">
+                    <input type="number" id="telefono" name="telefono" value="<?php echo $telefono?>" required class="inputs">
                 </div>
             </div>
             <div class="form-botones">
