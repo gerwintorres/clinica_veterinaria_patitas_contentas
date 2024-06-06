4f7edf2ae3dccead0bd755e6fa21d9ed2452713e
Camilo Castano <camilo.castano.casas@gmail.com>
2024-06-05 23:49:43 -0500

    Correción formulario para ingresar clientes desde el Admininistrador

--- front-end/admin/clientes/anadir_cliente.php
+++ front-end/admin/clientes/anadir_cliente.php
@@ -14,26 +14,7 @@
         $direccion = $_POST['direccion'];
         registrarCliente($nombre, $apellidos, $tipoDocumento, $numeroDocumento, $telefono, $correo, $password, $direccion);
     }
-
-    $urlAnterior = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-    $urlEsperada = "clientes.php"; 
-
-    $parsedUrl = parse_url($urlAnterior, PHP_URL_PATH);
-    $filename = basename($parsedUrl);
-    $mostrarBotonAtras = false;
-
-    if ($filename === $urlEsperada){
-        $mostrarBotonAtras = true;
-    }
 ?>
-
-<?php if ($mostrarBotonAtras): ?>
-    <div class="contenedor contenedor-boton-atras">
-        <a href="./clientes.php" class="boton-atras"> <svg width="11" height="20" viewBox="0 0 11 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.254358 13.9952C-0.0847876 13.5657 -0.0847876 12.8695 0.254358 12.44L9.10815 1.22798C9.4473 0.798517 9.99707 0.798517 10.3362 1.22798L10.7456 1.74641C11.0848 2.17588 11.0848 2.87218 10.7456 3.30166L2.9153 13.2176L10.7456 23.1337C11.0848 23.5631 11.0848 24.2593 10.7456 24.6888L10.3362 25.2073C9.99707 25.6368 9.4473 25.6368 9.10815 25.2073L0.254358 13.9952Z" fill="#231F57"/>
-        </svg>Ir atrás</a>
-    </div>
-<?php endif; ?>
 
 <h1 class="titulo-h1-pagina">Formulario de registro de cliente</h1>
 
