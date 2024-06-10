1bd7293c9bc83e44d4c39574607926bd5a84302a
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-06-09 21:20:04 -0500

    Pop ups Configuracion Perfil Cliente

--- front-end/cliente/configuracion/modificar_perfil.php
+++ front-end/cliente/configuracion/modificar_perfil.php
@@ -15,6 +15,11 @@
         $direccion = $_POST['direccion'];
         actualizarCliente($_SESSION['id_cliente'], $nombre, $apellidos, $telefono, $correo, $password, $direccion);
     }
+
+    if(isset($_SESSION['actualizado'])){
+        alertaActualizado('Usuario actualizado exitosamente');
+        unset($_SESSION['actualizado']);
+    }
 ?>
 
 <div class="contenedor contenedor-boton-atras">
